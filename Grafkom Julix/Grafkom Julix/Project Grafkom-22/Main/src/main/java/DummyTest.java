33cf6d91ef04e010249bc5a53d956ac6c40f328f
felix <c14210050@john.petra.ac.id>
2023-06-25 16:34:05 +0700

    environment

--- Grafkom Julix/Grafkom Julix/Project Grafkom-22/Main/src/main/java/DummyTest.java
+++ Grafkom Julix/Grafkom Julix/Project Grafkom-22/Main/src/main/java/DummyTest.java
@@ -37,6 +37,9 @@ public class DummyTest {
     Model m5 = null;
     Model m6 = null;
     Model m7 = null;
+    Model m8 = null;
+    Model m9 = null;
+    Model m10 = null;
     Model w1 = null;
     Model w2 = null;
     Model w3 = null;
@@ -82,6 +85,9 @@ public class DummyTest {
             m5 = ObjLoader.loadModel(new File("Grafkom Julix/Grafkom Julix/Project Grafkom-22/Main/src/blenderAssets/desert.obj"));
             m6 = ObjLoader.loadModel(new File("Grafkom Julix/Grafkom Julix/Project Grafkom-22/Main/src/blenderAssets/sun.obj"));
             m7 = ObjLoader.loadModel(new File("Grafkom Julix/Grafkom Julix/Project Grafkom-22/Main/src/blenderAssets/lampulorong.obj"));
+            m8 = ObjLoader.loadModel(new File("Grafkom Julix/Grafkom Julix/Project Grafkom-22/Main/src/blenderAssets/kota.obj"));
+            m9 = ObjLoader.loadModel(new File("Grafkom Julix/Grafkom Julix/Project Grafkom-22/Main/src/blenderAssets/bangunanskyscraper.obj"));
+            m10 = ObjLoader.loadModel(new File("Grafkom Julix/Grafkom Julix/Project Grafkom-22/Main/src/blenderAssets/tiangskyscraper.obj"));
             w1 = ObjLoader.loadModel(new File("Grafkom Julix/Grafkom Julix/Project Grafkom-22/Main/src/blenderAssets/walle_mata.obj"));
             w2 = ObjLoader.loadModel(new File("Grafkom Julix/Grafkom Julix/Project Grafkom-22/Main/src/blenderAssets/walle_kepala.obj"));
             w3 = ObjLoader.loadModel(new File("Grafkom Julix/Grafkom Julix/Project Grafkom-22/Main/src/blenderAssets/walle_badan.obj"));
@@ -100,56 +106,6 @@ public class DummyTest {
         }catch (IOException e){
             e.printStackTrace();
         }
-        //kepala eve
-        importX.add(new Sphere(
-                shaderModuleDataList,
-                new ArrayList<>(
-                ),
-                new Vector4f(1.0f, 1.0f, 1.0f, 1.0f),
-                0.0,
-                new ArrayList<>(List.of(0f, 0f, 0f)),
-                4.0f,
-                0.1f,
-                8.0f,
-                15,
-                30,
-                m));
-        importX.get(0).scaleObject(0.8f,0.8f,0.8f);
-        importX.get(0).translateObject(2f,2.5f,0f);
-
-        //muka hitam eve
-        importX.add(new Sphere(
-                shaderModuleDataList,
-                new ArrayList<>(
-                ),
-                new Vector4f(0f, 0f, 0f, 1.0f),
-                0.0,
-                new ArrayList<>(List.of(0f, 0f, 0f)),
-                4.0f,
-                0.1f,
-                8.0f,
-                15,
-                30,
-                m4));
-        importX.get(1).scaleObject(0.8f,0.8f,0.85f);
-        importX.get(1).translateObject(2f,2.5f,0f);
-        //mata eve
-        importX.add(new Sphere(
-                shaderModuleDataList,
-                new ArrayList<>(
-                ),
-                new Vector4f(153/255f, 255/255f, 255/255f, 1.0f),
-                0.0,
-                new ArrayList<>(List.of(0f, 0f, 0f)),
-                4.0f,
-                0.1f,
-                8.0f,
-                15,
-                30,
-                m1));
-        importX.get(2).translateObject(2.88f,2.05f,0.02f);
-        importX.get(2).scaleObject(0.9f,0.9f,0.9f);
-
         //badan dan tangan eve
         importX.add(new Sphere(
                 shaderModuleDataList,
@@ -164,7 +120,58 @@ public class DummyTest {
                 15,
                 30,
                 m2));
-        importX.get(3).translateObject(2f,0.2f,0f);
+        importX.get(0).translateObject(2f,0.2f,0f);
+        //kepala eve
+        importX.get(0).getChildObject().add(new Sphere(
+                shaderModuleDataList,
+                new ArrayList<>(
+                ),
+                new Vector4f(1.0f, 1.0f, 1.0f, 1.0f),
+                0.0,
+                new ArrayList<>(List.of(0f, 0f, 0f)),
+                4.0f,
+                0.1f,
+                8.0f,
+                15,
+                30,
+                m));
+        importX.get(0).getChildObject().get(0).scaleObject(0.8f,0.8f,0.8f);
+        importX.get(0).getChildObject().get(0).translateObject(2f,2.5f,0f);
+
+        //muka hitam eve
+        importX.get(0).getChildObject().add(new Sphere(
+                shaderModuleDataList,
+                new ArrayList<>(
+                ),
+                new Vector4f(0f, 0f, 0f, 1.0f),
+                0.0,
+                new ArrayList<>(List.of(0f, 0f, 0f)),
+                4.0f,
+                0.1f,
+                8.0f,
+                15,
+                30,
+                m4));
+        importX.get(0).getChildObject().get(1).scaleObject(0.8f,0.8f,0.85f);
+        importX.get(0).getChildObject().get(1).translateObject(2f,2.5f,0f);
+        //mata eve
+        importX.get(0).getChildObject().add(new Sphere(
+                shaderModuleDataList,
+                new ArrayList<>(
+                ),
+                new Vector4f(153/255f, 255/255f, 255/255f, 1.0f),
+                0.0,
+                new ArrayList<>(List.of(0f, 0f, 0f)),
+                4.0f,
+                0.1f,
+                8.0f,
+                15,
+                30,
+                m1));
+        importX.get(0).getChildObject().get(2).translateObject(2.88f,2.05f,0.02f);
+        importX.get(0).getChildObject().get(2).scaleObject(0.9f,0.9f,0.9f);
+
+
         //desert
         importX.add(new Sphere(
                 shaderModuleDataList,
@@ -179,7 +186,7 @@ public class DummyTest {
                 15,
                 30,
                 m5));
-        importX.get(4).scaleObject(10f,10f,10f);
+        importX.get(1).scaleObject(7f,7f,7f);
 
         //lorong
         importX.add(new Sphere(
@@ -195,7 +202,7 @@ public class DummyTest {
                 15,
                 30,
                 m3));
-        importX.get(5).translateObject(0f,0f,-1f);
+        importX.get(2).translateObject(0f,0f,-1f);
 
         //matahari
         importX.add(new Sphere(
@@ -211,9 +218,9 @@ public class DummyTest {
                 15,
                 30,
                 m6));
-        importX.get(6).translateObject(0f,15f,0f);
-        importX.get(6).scaleObject(3f,3f,3f);
-
+        importX.get(3).translateObject(0f,25f,0f);
+        importX.get(3).scaleObject(3f,3f,3f);
+        //lampu lorong
         importX.add(new Sphere(
                 shaderModuleDataList,
                 new ArrayList<>(
@@ -227,8 +234,8 @@ public class DummyTest {
                 15,
                 30,
                 m7));
-        importX.get(7).translateObject(1.852f,3.95f,4.0f);
-
+        importX.get(4).translateObject(1.852f,3.95f,4.0f);
+        //lampu lorong
         importX.add(new Sphere(
                 shaderModuleDataList,
                 new ArrayList<>(
@@ -242,88 +249,7 @@ public class DummyTest {
                 15,
                 30,
                 m7));
-        importX.get(8).translateObject(4.354f,3.95f,4.0f);
-
-        // mata walle
-        importX.add(new Sphere(
-                shaderModuleDataList,
-                new ArrayList<>(
-                ),
-                new Vector4f(0f, 0f, 0f, 1.0f),
-                0.0,
-                new ArrayList<>(List.of(0f, 0f, 0f)),
-                4.0f,
-                0.1f,
-                8.0f,
-                15,
-                30,
-                w1));
-        importX.get(9).translateObject(2.78f,1.3f,5.65f);
-        importX.get(9).scaleObject(0.8f,0.8f,0.8f);
-        //kepala walle
-        importX.add(new Sphere(
-                shaderModuleDataList,
-                new ArrayList<>(
-                ),
-                new Vector4f(224/255f, 224/255f, 224/255f, 1.0f),
-                0.0,
-                new ArrayList<>(List.of(0f, 0f, 0f)),
-                4.0f,
-                0.1f,
-                8.0f,
-                15,
-                30,
-                w2));
-        importX.get(10).translateObject(1.45f,-2f,10f);
-        importX.get(10).scaleObject(0.4f,0.4f,0.4f);
-        //badan walle
-        importX.add(new Sphere(
-                shaderModuleDataList,
-                new ArrayList<>(
-                ),
-                new Vector4f(255/255f, 128/255f, 0/255f, 1.0f),
-                0.0,
-                new ArrayList<>(List.of(0f, 0f, 0f)),
-                4.0f,
-                0.1f,
-                8.0f,
-                15,
-                30,
-                w3));
-        importX.get(11).translateObject(3f,-2f,12.5f);
-        importX.get(11).scaleObject(0.3f,0.3f,0.3f);
-        //tangan walle
-        importX.add(new Sphere(
-                shaderModuleDataList,
-                new ArrayList<>(
-                ),
-                new Vector4f(255/255f, 178/255f, 152/255f, 1.0f),
-                0.0,
-                new ArrayList<>(List.of(0f, 0f, 0f)),
-                4.0f,
-                0.1f,
-                8.0f,
-                15,
-                30,
-                w4));
-        importX.get(12).translateObject(14f,-2f,12f);
-        importX.get(12).scaleObject(0.3f,0.3f,0.3f);
-        //ban walle
-        importX.add(new Sphere(
-                shaderModuleDataList,
-                new ArrayList<>(
-                ),
-                new Vector4f(0f, 0f, 0f, 0f),
-                0.0,
-                new ArrayList<>(List.of(0f, 0f, 0f)),
-                4.0f,
-                0.1f,
-                8.0f,
-                15,
-                30,
-                w5));
-        importX.get(13).translateObject(2f,-2.5f,20f);
-        importX.get(13).scaleObject(0.3f,0.3f,0.3f);
+        importX.get(5).translateObject(4.354f,3.95f,4.0f);
 
         //YOEL BADAN
         importX.add(new Sphere(
@@ -430,6 +356,56 @@ public class DummyTest {
                 30,
                 y6));
         importX.get(14).getChildObject().get(2).getChildObject().get(0).translateObject(-1.66f,4.1f,-0.01f);
+        //bangunan
+        importX.add(new Sphere(
+                shaderModuleDataList,
+                new ArrayList<>(
+                ),
+                new Vector4f(128/255f, 128/255f, 128/255f, 1.0f),
+                0.0,
+                new ArrayList<>(List.of(0f, 0f, 0f)),
+                4.0f,
+                0.1f,
+                8.0f,
+                15,
+                30,
+                m8));
+        importX.get(8).translateObject(-19f,0f,10f);
+        //bangunan skyscraper
+        importX.add(new Sphere(
+                shaderModuleDataList,
+                new ArrayList<>(
+                ),
+                new Vector4f(128/255f, 128/255f, 128/255f, 1.0f),
+                0.0,
+                new ArrayList<>(List.of(0f, 0f, 0f)),
+                4.0f,
+                0.1f,
+                8.0f,
+                15,
+                30,
+                m9));
+        importX.get(9).translateObject(24f,0f,-20f);
+        importX.get(9).scaleObject(0.4f,0.4f,0.4f);
+
+        //tiang skyscraper
+        importX.add(new Sphere(
+                shaderModuleDataList,
+                new ArrayList<>(
+                ),
+                new Vector4f(183/255f, 65/255f, 14/255f, 1.0f),
+                0.0,
+                new ArrayList<>(List.of(0f, 0f, 0f)),
+                4.0f,
+                0.1f,
+                8.0f,
+                15,
+                30,
+                m10));
+        importX.get(10).translateObject(12f,0f,-20f);
+        importX.get(10).scaleObject(0.3f,0.3f,0.3f);
+
+
     }
 
     public void input() {
